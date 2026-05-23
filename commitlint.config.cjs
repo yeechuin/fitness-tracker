8fa43937e9e68aaf3b93d5746647f591131898fa
yeechuin <yeechuin6080@gmail.com>
2026-05-23 21:08:44 +0800

    feat: restructure project into frontend/backend architecture

--- commitlint.config.cjs
+++
@@ -1,9 +0,0 @@
-// commitlint.config.cjs
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    // optional: override/add rules
-    'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'style', 'refactor']],
-    'subject-case': [0] // allow any case
-  },
-};
